cb5a26047f6fc4ecd52c4eba12e5a78c319c44e9
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-09-24 20:32:03 +0200

    feat : set fov camera

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import fragment from "./shaders/fragment.glsl";
 import vertex from "./shaders/vertex.glsl";
-import testTexture from "./assets/water.jpg";
 
 export default class Sketch {
   constructor(options) {
@@ -10,12 +9,13 @@ export default class Sketch {
     this.width = this.container.offsetWidth;
     this.height = this.container.offsetHeight;
     this.camera = new THREE.PerspectiveCamera(
-      70,
+      30,
       this.width / this.height,
-      0.01,
-      10
+      10,
+      1000
     );
-    this.camera.position.z = 1;
+    this.camera.position.z = 600;
+    this.camera.fov = Math.atan(this.height / 2 / 600) * 2 * (180 / Math.PI);
     this.scene = new THREE.Scene();
 
     this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
@@ -41,12 +41,11 @@ export default class Sketch {
     window.addEventListener("resize", this.resize.bind(this));
   }
   addObjects() {
-    this.geometry = new THREE.PlaneGeometry(0.5, 0.5, 100, 100);
+    this.geometry = new THREE.PlaneGeometry(500, 200, 100, 100);
     this.material = new THREE.ShaderMaterial({
       // wireframe: true,
       uniforms: {
         time: { value: 1.0 },
-        uTexture: { value: new THREE.TextureLoader().load(testTexture) },
         resolution: { value: new THREE.Vector2() },
       },
       vertexShader: vertex,

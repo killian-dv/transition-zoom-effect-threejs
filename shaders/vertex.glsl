0c773cbe37a62a323453c5e8374ff37ddc1d9bc3
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-10-02 22:57:21 +0200

    feat : add waves animation

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -7,14 +7,17 @@ varying vec2 vSize;
 varying vec2 vUv;
 
 void main () {
+  float PI = 3.141592653589793238;
   vUv = uv;
+  float sine = sin(PI*uProgress);
+  float waves = sine*0.1*sin(5.*length(uv) + 10.*uProgress); // Modify the value multiplied to uProgress to change the number of the waves
   vec4 defaultState = modelMatrix*vec4( position, 1.0);
   vec4 fullSreenState = vec4( position, 1.0);
   fullSreenState.x *= uResolution.x/uQuadSize.x;
   fullSreenState.y *= uResolution.y/uQuadSize.y;
   float cornersProgress = mix(mix(uCorners.z, uCorners.w, uv.x), mix(uCorners.x, uCorners.y, uv.x), uv.y);
 
-  vec4 finalState = mix(defaultState, fullSreenState, cornersProgress);
+  vec4 finalState = mix(defaultState, fullSreenState, uProgress + waves);
 
   vSize = mix(uQuadSize, uResolution, uProgress);
   gl_Position = projectionMatrix * viewMatrix * finalState ;
